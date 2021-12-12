bdcdb7b2502bdfcae04c1a038376bc881f4acb04
Jimmy Moon <ragingwind@gmail.com>
2021-12-12 12:56:49 +0900

    Setup initializing code

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,23 @@
-#[allow(dead_code)]
-struct BengBenge {
+#![allow(unused_variables)]
+#![allow(dead_code)]
 
+pub struct BengBenge {
+    dns: String
 }
 
-#[allow(dead_code)]
-struct BengBenges {
-    benge: [BengBenge; 0]
-}
-
-#[allow(dead_code)]
-fn sum(a: i32, b: i32) -> i32 {
-    a + b
+impl BengBenge {
+    pub fn new(dns: String) -> BengBenge {
+        BengBenge {dns}
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::sum;
-
+    use super::*;
+    
     #[test]
     fn it_works() {
-        // assert_eq!(sum(2, 2), 4);
+        let bb = BengBenge::new("bengbenge".to_string());
+        assert_eq!(bb.dns, "bengbenge");
     }
 }
-
