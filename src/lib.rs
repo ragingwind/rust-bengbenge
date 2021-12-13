44f3db711a9788786b4b9c7e4e4ebf1cdd086eb3
Jimmy Moon <ragingwind@gmail.com>
2021-12-13 22:44:25 +0900

    Use Iterait Traits

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,43 @@
-#![allow(unused_variables)]
 #![allow(dead_code)]
 
-pub struct BengBenge {
-    dns: String
+use std::slice::Iter;
+
+struct BengBenge {
+    container: Vec<String>,
+    cursor: usize,
 }
 
 impl BengBenge {
-    pub fn new(dns: String) -> BengBenge {
-        BengBenge {dns}
+    fn new() -> BengBenge {
+        BengBenge{
+            container: Vec::new(),
+            cursor: 0,
+        }
+    }
+
+    fn append(&mut self, value: String) {
+        self.container.push(value);
+    }
+
+    fn iter(&self) -> Iter<String> {
+        self.container.iter()
+    }
+}
+
+impl Iterator for BengBenge {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.container.get(self.cursor) {
+            Some(value) => {
+                self.cursor += 1;
+                Some(value.to_owned())
+            },
+            None => {
+                self.cursor = 0;
+                Some(self.container.get(self.cursor).unwrap().to_owned())
+            },
+        }
     }
 }
 
@@ -17,7 +47,18 @@ mod tests {
     
     #[test]
     fn it_works() {
-        let bb = BengBenge::new("bengbenge".to_string());
-        assert_eq!(bb.dns, "bengbenge");
+        let mut bb = BengBenge::new();
+
+        bb.append("192.168.0.1".to_string());
+        bb.append("192.168.0.2".to_string());
+        bb.append("192.168.0.3".to_string());
+        bb.append("192.168.0.4".to_string());
+
+        let mut iterator = bb.iter();
+
+        assert_eq!("192.168.0.1", iterator.next().unwrap());
+        assert_eq!("192.168.0.2", iterator.next().unwrap());
+        assert_eq!("192.168.0.3", iterator.next().unwrap());
+        assert_eq!("192.168.0.4", iterator.next().unwrap());
     }
 }
