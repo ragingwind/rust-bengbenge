9befbd13a5ff45a022ecc8f5a2d0fa09e4d3e27c
Jimmy Moon <ragingwind@gmail.com>
2022-01-03 11:23:17 +0900

    Add cache

--- scripts/act.sh
+++ scripts/act.sh
@@ -2,4 +2,4 @@ if [[ $(uname -m) == 'arm64' ]]; then
   CONTAINER_ARCH="--container-architecture linux/amd64"
 fi
 
-act -j $1 $CONTAINER_ARCH --env-file act.env
+act -j $1 $CONTAINER_ARCH --env-file ./scripts/act.env
